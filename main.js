c9afb4297db6275894957349fdf657dcfcb9b689
Oren <ogolan@yp.com>
2014-11-02 23:55:27 -0800

    init

---
+++ main.js
@@ -0,0 +1,17 @@
+var tmpl = require('microtemplates');
+var fs = require('fs');
+
+document.addEventListener('DOMContentLoaded', function() {
+  var $ = document.querySelector.bind(document);
+
+  var users = $("#users");
+  var dataObject = {
+                     id: 1,
+                     profile_image_url: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTgsVCUkn2gEZWhqpKMfZQoWx1uQfKBOM4RtdAHYrFoTbdQPMJw',
+                     from_user: 'josh',
+                     text: 'hello'
+                   };
+
+  var itemTmpl = fs.readFileSync('item.html', 'utf8');
+  users.innerHTML = tmpl(itemTmpl)(dataObject);
+});
---
+++ readme.md
@@ -0,0 +1,25 @@
+# micro template usage
+
+This project is demonstrating how to use the microtemplate module.  
+Microtemplate is a small render function that is based on http://ejohn.org/blog/javascript-micro-templating/#postcomment
+
+I use the 'fs' module to read the template file into a variable at build-time.
+
+## install
+
+* [Install](#install)
+* [Watch and complie browserify](#watch)
+* [Run](#run)
+
+## Install
+
+    npm install
+
+## Watch and compile browserify
+
+    npm run build
+
+## Run
+
+    open index.html in the browser
+
